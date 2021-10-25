095700e1d92ccb8b73a6aec83104ca58a363667b
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-10-25 16:08:55 +0530

    -

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
+const sideNav = document.getElementById('mySidenav');
+const title = document.getElementById('title');
+const knowMore = document.getElementById('know-more');
+
 const openNav = () => {
-    document.getElementById("mySidenav").style.width = "250px";
+    sideNav.style.width = '250px';
 }
 const closeNav = () => {
-  document.getElementById("mySidenav").style.width = "0";
+  sideNav.style.width = '0';
 }
+const hover = () => {
+  if(window.innerWidth >= 1020){
+    title.innerHTML = "<span id=\"mm\">Madhan Aadithya</span>"
+    title.style.fontSize = "6.6458333333333335vw"
+  }
+  else{
+    title.innerHTML = "<span id=\"mm\">M</span>adhan"
+  }
+}
+const leave = () => {
+  if(window.innerWidth >= 1020){
+    title.innerHTML = "<span id=\"mm\">M</span>adhan"
+    title.style.fontSize = "10.6458333333333335vw"
+  }
+  else{
+    title.innerHTML = "<span id=\"mm\">M</span>adhan"
+  }
+}
+knowMore.addEventListener("mouseover", hover);
+knowMore.addEventListener("mouseleave", leave);
