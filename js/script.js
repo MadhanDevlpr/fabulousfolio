e18c0e13d9d23b4d748636d8b368b43cf6823c4e
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-10-26 15:27:53 +0530

    Fabulous

--- js/script.js
+++ js/script.js
@@ -10,20 +10,20 @@ const closeNav = () => {
 }
 const hover = () => {
   if(window.innerWidth >= 1020){
-    title.innerHTML = "<span id=\"mm\">Madhan Aadithya</span>"
+    title.innerHTML = "<span id=\"mm\">Fabulous Folio</span>"
     title.style.fontSize = "8.6458333333333335vw"
   }
   else{
-    title.innerHTML = "<span id=\"mm\">M</span>adhan"
+    title.innerHTML = "<span id=\"mm\">F</span>abulous"
   }
 }
 const leave = () => {
   if(window.innerWidth >= 1020){
-    title.innerHTML = "<span id=\"mm\">M</span>adhan"
+    title.innerHTML = "<span id=\"mm\">F</span>abulous"
     title.style.fontSize = "10.6458333333333335vw"
   }
   else{
-    title.innerHTML = "<span id=\"mm\">M</span>adhan"
+    title.innerHTML = "<span id=\"mm\">F</span>abulous"
   }
 }
 knowMore.addEventListener("mouseover", hover);
