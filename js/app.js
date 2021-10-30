4a5eeef53976d0dac9d356dc0c50a021865d7b98
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-10-30 20:18:30 +0530

    New header anime

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 2080,
+        "value": 1080,
         "density": {
           "enable": true,
           "value_area": 800
